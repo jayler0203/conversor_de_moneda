23bef5044152210f8815f1a3d293d1f477a049c3
Jayler Castillo <jayler0203@gmail.com>
2025-06-16 11:39:54 -0500

    Update Main class to fetch and print exchange rate data

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,13 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
+        ApiService apiService = new ApiService();
+        try {
+            ExchangeResponse data = apiService.getData();
+            System.out.println(data);
+        } catch (Exception e) {
+            System.out.println("Error al obtener los datos" + e.getMessage());
+        }
 
     }
+
 }
